41056b53f1645dfd7c65aedbb8798a6b5959e451
Ciro Rezende <cirorezende@gmail.com>
2018-11-13 18:17:04 -0200

    camadas

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,24 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.service.CategoriaService;
 
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	CategoriaService service;
 
-		List<Categoria> categorias = new ArrayList<>();
-
-		categorias.add(new Categoria(1, "Informática"));
-		categorias.add(new Categoria(2, "Escritório"));
-
-		return categorias;
-
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		return ResponseEntity.ok(service.get(id));
 	}
 
 }
---
+++ src/main/java/com/nelioalves/cursomc/service/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.nelioalves.cursomc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	CategoriaRepository repository;
+
+	public Categoria get(Integer id) {
+		return repository.findById(id).orElse(null);
+	}
+
+}
