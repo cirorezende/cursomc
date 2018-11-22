52cb7ee282e53dd507e8a17680b6ae7d158d84cd
Ciro Rezende <cirorezende@gmail.com>
2018-11-22 19:24:24 -0200

    controller para cliente

---
+++ src/main/java/com/nelioalves/cursomc/service/ClienteService.java
@@ -0,0 +1,20 @@
+package com.nelioalves.cursomc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repository.ClienteRepository;
+import com.nelioalves.cursomc.service.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	ClienteRepository repository;
+
+	public Cliente get(Integer id) throws ObjectNotFoundException {
+		return repository.findById(id).orElseThrow( () -> new ObjectNotFoundException("Cliente nao encontrado. Id: " + id ) ); 
+	}
+
+}
