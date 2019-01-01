4ee89443d712a0d74379af653347cdaa62cf6a10
Ciro Rezende <cirorezende@gmail.com>
2019-01-01 21:27:34 -0200

    ListALl com DTO Categorias

--- src/main/java/com/nelioalves/cursomc/service/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/service/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -36,6 +38,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 
+	}
+
+	public List<Categoria> findAll() {
+		return repository.findAll();
 	}
 
 }
